332dc2ff44326f89f3a52b0c53f1eaaf249b86c2
EmDev16 <emmanuel.sabu.lubunza@student.ehb.be>
2026-01-08 00:04:15 +0100

    Huidige project setup met Node en gitignore. Express server setup in app.js met de basis (middelware, routes, static files en server starter).

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import eventsRoutes from './routes/events.js';
+import locationsRoutes from './routes/locations.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.use(express.json());
+
+app.use("api/events", eventsRoutes);
+app.use("api/locations", locationsRoutes);
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
