7be17b9fea24a2dc180c047aa3f7758ff51387b9
EmDev16 <emmanuel.sabu.lubunza@student.ehb.be>
2026-01-09 01:29:40 +0100

    kleine route bugs gefixed

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 import express from 'express';
-import eventsRoutes from './routes/events.js';
-import locationsRoutes from './routes/locations.js';
+import locationsRouter from './routes/locations.js';
+import eventsRouter from './routes/events.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
 app.use(express.json());
 
-app.use('/api/events', eventsRoutes);
-app.use('/api/locations', locationsRoutes);
+app.use('/api/locations', locationsRouter);
+app.use('/api/events', eventsRouter);
 
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
